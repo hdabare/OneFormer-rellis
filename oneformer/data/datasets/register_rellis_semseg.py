3e3d7442de0c6cb85dea015724e9b2bc8de87c08
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-03 14:01:53 +0800

    Remove unused import

--- oneformer/data/datasets/register_rellis_semseg.py
+++ oneformer/data/datasets/register_rellis_semseg.py
@@ -1,17 +1,10 @@
-import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
 # import some common libraries
-import numpy as np
-import os, json, cv2, random
-from google.colab.patches import cv2_imshow
+import os, cv2
 
 # import some common detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
 def get_rellis_dicts(img_dir, split_filename):
