1b7a259299a285e1e86283b198c04d4c009e0079
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-31 16:58:11 +0800

    Add pillow image to demo

--- demo/demo.py
+++ demo/demo.py
@@ -17,7 +17,7 @@ import time
 import cv2
 import numpy as np
 import tqdm
-
+from PIL import Image
 import torchvision.utils as tutils
 
 from detectron2.config import get_cfg
@@ -159,7 +159,8 @@ if __name__ == "__main__":
                         pred = predictions["sem_seg"].argmax(dim=0).to(demo.cpu_device, torch.uint8).numpy()
                         out = wanerie_seq_to_raw(pred)
                         cv2.imwrite(out_filename.replace("out.png", "pred.png"), pred)
-                        cv2.imwrite(out_filename.replace("out.png", "pred_color.png"), out)
+                        Image.fromarray(out.astype(np.uint8)).save(out_filename.replace("out.png", "pred_color.png"))
+                        # cv2.imwrite(, out)
                         visualized_output[k].save(out_filename)    
                 else:
                     for k in visualized_output.keys():
